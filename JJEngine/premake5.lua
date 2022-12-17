399a9dbdb05638ba28d4ca02a378c72cae783074
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2022-12-16 19:50:00 -0800

    project settings

---
+++ JJEngine/premake5.lua
@@ -0,0 +1,86 @@
+workspace "JJEngine"
+    platforms {"Win64"}
+    configurations  { "Debug", "Release" }    
+    startproject "Game"
+
+IncludeDir={}
+
+
+
+project "JJEngine"
+    location "JJEngine"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "c++17"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "bin/%{cfg.buildcfg}-obj"
+    architecture "x86_64"
+    links {
+
+    }
+
+    disablewarnings {}
+    linkoptions {}
+    includedirs
+    {
+    }
+    files {
+        "JJEngine/**.h",
+        "JJEngine/**.cpp"
+    }
+    defines{
+        "GLFW_INCLUDE_NONE",
+        "_CRT_SECURE_NO_WARNINGS"
+    }
+
+    postbuildcommands {
+
+    }
+
+
+    -- All of these settings will appear in the Debug configuration
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+project "Game"
+    location "Game"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "c++17"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "bin/%{cfg.buildcfg}-obj"
+    architecture "x86_64"
+    links {
+    
+    }
+    
+    disablewarnings {}
+    linkoptions {}
+    includedirs
+    {
+    }
+    files {
+        "Game/**.h",
+        "Game/**.cpp"
+    }
+    defines{
+        "GLFW_INCLUDE_NONE",
+        "_CRT_SECURE_NO_WARNINGS"
+    }
+    
+    postbuildcommands {
+    
+    }
+    
+    
+    -- All of these settings will appear in the Debug configuration
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
