47a71d4609b11c28344240507bfbf20fdc358906
junsujang-digipen <65064387+junsujang-digipen@users.noreply.github.com>
2023-05-10 23:18:31 -0700

    Editor scene test

---
+++ JJEngine/Editor/TestScene.h
@@ -0,0 +1,142 @@
+#pragma once
+#include "Core/Scene.h"
+#include <string>
+#include <iostream>
+
+#include "glad.h"
+#include "Core/Graphics/Shader.h"
+#include "Core/Graphics/VertexArray.h"
+#include "Core/Graphics/VertexBuffer.h"
+
+// for testing component manager
+#include "Core/ComponentManager.h"
+#include "Core/Graphics/Texture.h"
+#include "Core/Utils/File.h"
+#include "imgui.h"
+#include "Core/Graphics/FrameBuffer.h"
+#include "Core/Graphics/IndexBuffer.h"
+
+class TestScene : public Scene
+{
+	std::string text{};
+	std::shared_ptr<VertexArray> vertex_array;
+	std::shared_ptr<VertexBuffer> vertex_buffer;
+	std::shared_ptr<IndexBuffer> index_buffer;
+	std::shared_ptr<VertexBuffer> vertex_color_buffer;
+	std::shared_ptr<FrameBuffer> framebuffer;
+	std::shared_ptr<Shader> shader;
+	std::shared_ptr<Texture> test_texture;
+public:
+	TestScene(std::string);
+	~TestScene();
+	void Load() override
+	{
+		std::cout << text << std::endl;
+		shader = Shader::CreateShaderFromFile({
+	{ ShaderType::VertexShader,{"Resources/Shaders/version.glsl","Resources/Shaders/simple.vert"}},
+	{ ShaderType::FragmentShader,{"Resources/Shaders/version.glsl","Resources/Shaders/simple.frag"} }
+			});
+		shader->Use();
+		float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+		};
+		float colors[] = {
+			// colors
+			1.0f, 0.0f, 0.0f,   // bottom right
+			0.0f, 1.0f, 0.0f,   // bottom left
+			0.0f, 0.0f, 1.0f    // top 
+		};
+		float verticesWithColor[] = {
+			// positions         // colors
+			 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+			-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+			 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+		};
+
+		float textureQuad[] = {
+			// positions        // texture coords
+			 0.5f,  0.5f, 0.0f, 1.0f, 1.0f,   // top right
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,   // bottom left
+			0.5f, -0.5f, 0.0f, 1.0f, 0.0f,   // bottom right
+			-0.5f,  0.5f, 0.0f, 0.0f, 1.0f,    // top left 
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,   // bottom left
+			 0.5f,  0.5f, 0.0f, 1.0f, 1.0f,   // top right
+		};
+
+		float textureQuadIndexed[] = {
+			// positions        // texture coords
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,   // bottom left
+			0.5f, -0.5f, 0.0f, 1.0f, 0.0f,   // bottom right
+			0.5f,  0.5f, 0.0f, 1.0f, 1.0f,   // top right
+			-0.5f,  0.5f, 0.0f, 0.0f, 1.0f,    // top left 
+		};
+
+		vertex_array = VertexArray::CreateVertexArray();
+		vertex_array->Bind();
+		vertex_buffer = VertexBuffer::CreateVertexBuffer(sizeof(textureQuadIndexed));
+		vertex_buffer->SetData(sizeof(textureQuadIndexed), textureQuadIndexed);
+		vertex_buffer->SetDataTypes({
+			{0, DataType::Float3},//location=0, pos
+			{1, DataType::Float2},//location=1, uv
+			});
+		vertex_buffer->BindToVertexArray();
+
+
+		unsigned int textureQuadIndices[] = {
+			0,1,2,
+			0,2,3,
+		};
+
+		index_buffer = IndexBuffer::CreateIndexBuffer(sizeof(textureQuadIndices));
+		index_buffer->SetData(sizeof(textureQuadIndices), textureQuadIndices);
+		index_buffer->BindToVertexArray();
+		//vertex_color_buffer = VertexBuffer::CreateVertexBuffer(sizeof(colors));
+		//vertex_color_buffer->SetData(sizeof(colors), colors);
+		//vertex_color_buffer->SetDataTypes({ {1,DataType::Float3} });
+		//vertex_color_buffer->BindToVertexArray();
+
+
+		test_texture = Texture::CreateTexture(File::ReadImageToTexture("Resources/Textures/test.jpg"));
+
+		framebuffer = FrameBuffer::CreateFrameBuffer({ 1200, 800, { FrameBufferFormat::RGB } });
+		glClearColor(0.3, 0.7, 0.3, 1.0);
+	};
+	void Update(double dt) override
+	{
+
+		Scene::Update(dt);
+		framebuffer->Bind();
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+		//draw test
+		shader->Use();
+		vertex_array->Bind();
+		shader->SetInt("testTexture", 0);
+		test_texture->BindTexture();
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		framebuffer->UnBind();
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		//for imgui test and framebuffer
+		ImGui::Begin("framebuffer test");
+		constexpr ImVec2 size{ 480,320 };
+		unsigned textureID = framebuffer->GetColorTexture(0)->GetTextureID();
+		ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(textureID)), size, ImVec2{ 0,1 }, ImVec2{ 1,0 });
+		ImGui::End();
+	}
+	void Draw() override {}
+	void Unload()  override {}
+private:
+
+};
+
+TestScene::TestScene(std::string t) :Scene(), text(t)
+{
+
+}
+
+TestScene::~TestScene()
+{
+}
--- JJEngine/Editor/main.cpp
+++ JJEngine/Editor/main.cpp
@@ -1,5 +1,45 @@
+#include <Core/Window.h>
+#include <Core/SceneManager.h>
+#include <Core/ImGui/ImGuiRenderer.h>
+#include "TestScene.h"
 
 int main(void)
 {
+	// Editor engine??
+
+	Window* window = new Window{};
+	SceneManager* sceneManager = new SceneManager{};
+	ImGuiRenderer::Instance()->Init(window->GetGLFWWindow());
+	TestScene test1{ " editor eeeeeeeeeeeeeeee " };
+	TestScene test2{ " editor eeeeeeeeeeeeeeee 2" };
+	sceneManager->enrollScene(0, test1);
+	sceneManager->enrollScene(1, test2);
+	sceneManager->setNextScene(1);
+	bool engineLoop{ true };
+	std::chrono::system_clock::time_point lastTick = std::chrono::system_clock::now();
+	double dt{ };
+	do
+	{
+		const std::chrono::time_point now{ std::chrono::system_clock::now() };
+		dt = { std::chrono::duration<double>(now - lastTick).count() };
+		if (window->shouldClose() == true) {
+			sceneManager->exit();
+			engineLoop = false;
+		}
+
+		window->update([&]()
+			{
+				ImGuiRenderer::Instance()->GuiBegin();
+				sceneManager->update(dt);
+				ImGuiRenderer::Instance()->GuiEnd();
+			});
+
+		lastTick = now;
+
+	} while (engineLoop);
+
+
+	ImGuiRenderer::Instance()->Shutdown();
+
 	return 0;
 }
