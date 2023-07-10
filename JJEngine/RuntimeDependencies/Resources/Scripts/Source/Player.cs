08cf9ff28e8000ca28400c90d006a27276d13c94
daehyeon-digiPen <daehyeon.kim@digipen.edu>
2023-07-10 13:14:15 -0700

    add game

--- JJEngine/RuntimeDependencies/Resources/Scripts/Source/Player.cs
+++ JJEngine/RuntimeDependencies/Resources/Scripts/Source/Player.cs
@@ -10,19 +10,65 @@ namespace Game
     {
         private TransformComponent m_transform;
         private RigidBody2DComponent m_rigidBody;
-        
+
+        private int i = 0;
+
         public float Speed =60.0f;
         public float ttttt = 3.0f;
+        Vector3 velocity = Vector3.Zero;
+
         protected override void OnCreate()
         {
             Console.WriteLine($"Player create");
             m_transform = GetComponent<TransformComponent>();
             m_rigidBody = GetComponent<RigidBody2DComponent>();
+
+            Vector3 wallPos;
+            Entity entity = Scene.CreateEntity("Test" + i++.ToString());
+            entity.AddComponent<SpriteRendererComponent>();
+            wallPos.X = 1.0f;
+            wallPos.Y = -2.0f;
+            wallPos.Z = 0.0f;
+            entity.Position = wallPos;
+
+            //Give class "Wall"
+            //auto & script = entity.AddComponent<ScriptComponent>();
+            //script.Name = "Game.Player";
+
+
+            entity = Scene.CreateEntity("Test" + i++.ToString());
+            entity.AddComponent<SpriteRendererComponent>();
+            wallPos.X = 1.0f;
+            wallPos.Y = 2.0f;
+            wallPos.Z = 0.0f;
+            entity.Position = wallPos;
+
+            entity = Scene.CreateEntity("Test" + i++.ToString());
+            entity.AddComponent<SpriteRendererComponent>();
+            wallPos.X = 3.0f;
+            wallPos.Y = -2.0f;
+            wallPos.Z = 0.0f;
+            entity.Position = wallPos;
+
+            //Give class "Wall"
+            //auto & script = entity.AddComponent<ScriptComponent>();
+            //script.Name = "Game.Player";
+
+
+            entity = Scene.CreateEntity("Test" + i++.ToString());
+            entity.AddComponent<SpriteRendererComponent>();
+            wallPos.X = 3.0f;
+            wallPos.Y = 2.0f;
+            wallPos.Z = 0.0f;
+            entity.Position = wallPos;
         }
 
         protected override void OnUpdate()
         {
-            Vector3 velocity=Vector3.Zero;
+            float gravity = 0.3f;
+
+
+
             if (Input.IsPressed(KeyCode.W))
             {
                 velocity.Y += 1.0f;
@@ -45,14 +91,36 @@ namespace Game
                 velocity.X -= 1.0f;
             }
 
-            if (Input.IsPressed(KeyCode.Space))
+            if(Input.IsTriggered(KeyCode.Space))
             {
-                Entity entity = Scene.CreateEntity("Test");
-                entity.AddComponent<SpriteRendererComponent>();
-                entity.Position = Position;
+                velocity.Y = 8.0f;
             }
 
-            velocity *= Speed;
+            if (Input.IsPressed(KeyCode.Space))
+            {
+                Vector3 wallPos;
+                Entity entity = Scene.CreateEntity("Test" + i++.ToString());
+                entity.AddComponent<SpriteRendererComponent>();
+                wallPos.X = 2.0f;
+                wallPos.Y = -2.0f;
+                wallPos.Z = 0.0f;
+                entity.Position = wallPos;
+
+                //Give class "Wall"
+                //auto & script = entity.AddComponent<ScriptComponent>();
+                //script.Name = "Game.Player";
+
+
+                entity = Scene.CreateEntity("Test" + i++.ToString());
+                entity.AddComponent<SpriteRendererComponent>();
+                wallPos.X = 2.0f;
+                wallPos.Y = 2.0f;
+                wallPos.Z = 0.0f;
+                entity.Position = wallPos;
+            }
+
+            velocity.Y -= gravity;
+
             /*if (m_rigidBody != null)
             {
                 m_rigidBody.ApplyLinearImpulse(velocity.XY, true);
